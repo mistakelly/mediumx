af829d3f988680b83f160690127214ea12917a39
Kelvin Okoye <kelvinokoye413@gmail.com>
2025-04-14 13:09:29 +0100

    Implemented Private header component

---
+++ src/components/private/PrivateHeader.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import { InputComponent } from "../public/auth/ChooseStoryCategory";
+import { WriteSvg } from "../svg/WriteSvg";
+import searchSvg from "@/components/svg/search-magnifying-glass-svgrepo-com.svg";
+import { NotificaitonSvg } from "../svg/NotificationSvg";
+import "@/styles/private/private_header.scss";
+
+export const PrivateHeader = () => {
+  return (
+    <header className="private__header">
+      <div className="private__header-left">
+        <h1>
+          <Link to="/" />
+          MEDIUMX
+        </h1>
+        <InputComponent
+          name="search"
+          placeholder="search"
+          type="text"
+          icon={searchSvg}
+          imgAlt=""
+        />
+      </div>
+      <div className="private__header-right">
+        <div className="div">
+          <Link to="/new-story">
+            <WriteSvg />
+          </Link>
+        </div>
+
+        <NotificaitonSvg />
+        <div className="right-profile-div">
+          <img src="/assets/images/test_img.png" alt="profile-img" />
+        </div>
+      </div>
+    </header>
+  );
+};
